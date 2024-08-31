43bcf40c4326757ae84ac1c5823e96a03fedeaa6
Sara Souza <105318470+sarasza@users.noreply.github.com>
2024-08-31 01:04:17 -0300

    Create precoMetroQuadrado1.cs

---
+++ precoMetroQuadrado1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = 10.0;
+            double length = 30.0;
+            double area = width * length;
+
+            double pricePerSquareMeter = 200.0;
+            double priceTotal = area * pricePerSquareMeter;
+
+            Console.WriteLine("A área total é: " + area.ToString("F2"), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("O valor do terreno é: " + priceTotal.ToString("F2"), CultureInfo.InvariantCulture) ;
+
+            Console.ReadLine();
+        }
+    }
+}
