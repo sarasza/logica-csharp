e0c142022bfa1ed89e67af85b8dc6ba7f5e0c7dd
Sara Souza <105318470+sarasza@users.noreply.github.com>
2024-08-31 00:35:07 -0300

    Create beecrowdExercicio1159.cs

---
+++ while-cs/beecrowdExercicio1159.cs
@@ -0,0 +1,31 @@
+/* O programa deve ler um valor inteiro X indefinidas vezes. (O programa irá parar quando o valor de X for igual a 0). Para cada X lido, imprima a soma dos 5 pares
+   consecutivos a partir de X, inclusive o X , se for par. Se o valor de entrada for 4, por exemplo, a saída deve ser 40, que é o resultado da operação: 4+6+8+10+12,
+   enquanto que se o valor de entrada for 11, por exempo, a saída deve ser 80, que é a soma de 12+14+16+18+20. */
+
+using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int media = 0;
+
+            while (number != 0)
+            {
+                if (number % 2 != 0)
+                {
+                    ++number;
+                }
+
+                media = number + number + 2 + number + 4 + number + 6 + number + 8;
+                Console.WriteLine(media);
+
+                number = int.Parse(Console.ReadLine());
+            }   
+
+        }
+    }
+}
